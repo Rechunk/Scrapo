404567a65288211b4499ad8a99281515d8bceff2
Rechunk <gabri.marc@gmx.net>
2017-07-17 12:00:40 +0200

    Generator functions called by the parser

--- src/generator.kt
+++ src/generator.kt
@@ -1,3 +1,27 @@
 /**
  * Created by User on 16.07.2017.
  */
+import org.openqa.selenium.WebDriver
+import java.awt.Desktop
+import java.net.URI
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+
+
+class Generator {
+    fun openWebsite(url: String){
+
+        /*if (Desktop.isDesktopSupported()){
+            Desktop.getDesktop().browse(URI(url))
+        }*/
+
+        val driver: WebDriver = ChromeDriver()
+
+        driver.get("https://google.com")
+
+    }
+
+    fun getWebsiteSourceCode(url: String){
+        val driver: WebDriver = PhantomJSDriver()
+    }
+}
