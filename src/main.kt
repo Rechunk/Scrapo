771eda3f4f081eaf000fd173c6be0708732fd332
Rechunk <gabri.marc@gmx.net>
2017-07-17 19:26:38 +0200

    Calls file with .sp extension

--- src/main.kt
+++ src/main.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>){
     val lexer: Lexer = Lexer()
     val parser: Parser = Parser()
 
-    val file: File = File("C:\\Users\\User\\Desktop\\Scrapo\\src\\code.txt")
+    val file: File = File("C:\\Users\\User\\Desktop\\Scrapo\\src\\code.sp")
     val tokens = lexer.lex(file.readText() + " ")
     parser.parse(tokens)
 
