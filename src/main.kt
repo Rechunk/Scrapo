99526482ef3248895bd29461e538680a43f31474
Rechunk <gabri.marc@gmx.net>
2017-07-18 11:23:56 +0200

    Print location changed

--- src/main.kt
+++ src/main.kt
@@ -6,8 +6,10 @@ fun main(args: Array<String>){
 
     val file: File = File("C:\\Users\\User\\Desktop\\Scrapo\\src\\code.sp")
     val tokens = lexer.lex(file.readText() + " ")
+
     parser.parse(tokens)
 
     tokens.forEach { println(it.value) }
+
 
 }
