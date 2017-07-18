1bc4d8f88c338f773cd7dd04cbe2e49dccf9defa
Rechunk <gabri.marc@gmx.net>
2017-07-18 15:27:09 +0200

    Click functionality added

--- src/parser.kt
+++ src/parser.kt
@@ -1,8 +1,3 @@
-import kotlin.reflect.jvm.internal.impl.serialization.deserialization.FlexibleTypeDeserializer
-
-/**
- * Created by User on 15.07.2017.
- */
 
 class Parser {
 
@@ -10,22 +5,35 @@ class Parser {
 
         val generator: Generator = Generator()
 
+        fun isConstant(word: String) : Boolean {
+            // TODO: Modularize to check for other constants
+            if (word == "BROWSER"){
+                return true
+            }
+            return false
+        }
+
         fun handleWord(value: String, i: Int){
             when (tokens[i].value){
                 "OPEN" -> {
                     generator.openWebsite(generator.driver!!, tokens[i+1].value)
                 }
-                "GET" -> {
-                    generator.getWebsiteSourceCode(tokens[i+1].value)
+                "CLOSE" -> {
+                    generator.closeWebbrowser(generator.driver!!)
                 }
                 "CLICK" -> {
                     when (tokens[i+1].value){
                         "CLASS" -> {
-                            // tokens[i+2] is the class to find and click on
+                            generator.clickOnClass(generator.driver!!, tokens[i+2].value)
                         }
                         "ID" -> {
                             // tokens[i+2] is the id to find and click on
                         }
+                    }
+                }
+                else -> {
+                    if (!isConstant(tokens[i].value)){
+                        throw KeywordNotFoundException("'${tokens[i].value}' could not be found!")
                     }
                 }
             }
@@ -45,9 +53,6 @@ class Parser {
                     }
                 }
                 TokenType.STRING -> {
-
-                }
-                else -> {
 
                 }
             }
