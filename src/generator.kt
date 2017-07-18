edfe09e81d198b15d72d189e992a6ccb94483d51
Rechunk <gabri.marc@gmx.net>
2017-07-18 11:22:54 +0200

    ...

--- src/generator.kt
+++ src/generator.kt
@@ -1,12 +1,6 @@
-/**
- * Created by User on 16.07.2017.
- */
+
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.phantomjs.PhantomJSDriver
-import kotlin.system.exitProcess
-
 
 class Generator {
 
