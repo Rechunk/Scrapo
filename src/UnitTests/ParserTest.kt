7b86169cb7a01728728d927cc57492bad1945a5d
Rechunk <gabri.marc@gmx.net>
2017-07-19 12:41:10 +0200

    Testing parser with mock-service

--- src/UnitTests/ParserTest.kt
+++ src/UnitTests/ParserTest.kt
@@ -5,7 +5,23 @@ import org.junit.Assert.*
 class ParserTest {
     @Test
     fun parse() {
-        assertEquals(4, 4)
+        val lexer: Lexer = Lexer()
+        val tokens = lexer.lex("""
+        BROWSER="CHROME"
+        OPEN "http://localhost:1234/automationsite/"
+        CLICK[0] CLASS "create-account-btn"
+        CLOSE
+""")
+
+        val parser: Parser = Parser()
+
+        parser.parse(tokens)
+        val data = parser.testingData
+
+        assertEquals(data[0], "SET-BROWSER-CHROME")
+        assertEquals(data[1], "OPEN-http://localhost:1234/automationsite/")
+        assertEquals(data[2], "CLICK[0]-CLASS-create-account-btn")
+        assertEquals(data[3], "CLOSE")
     }
 
 }
